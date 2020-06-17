356a4c7e05b70f1e05ecddaf479e44ae6cf352a7
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-17 21:00:11 +0300

    Implement Profile, Record
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/router/index.js
+++ crm-system-vue/src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Record from '../views/Record.vue'
 import History from '../views/History.vue'
+import Profile from '../views/Profile.vue'
 import Planning from '../views/Planning.vue'
 import Register from '../views/Register.vue'
 import Categories from '../views/Categories.vue'
@@ -52,6 +54,18 @@ const routes = [
     name: 'planning',
     meta: { layout: 'main' },
     component: Planning
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: Profile
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: Record
   }
 ]
 
