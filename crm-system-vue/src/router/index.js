eacd523c393822deb2e6020498220181fc98e80c
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-17 20:52:48 +0300

    Implement Home, History, Planning
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/router/index.js
+++ crm-system-vue/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import History from '../views/History.vue'
+import Planning from '../views/Planning.vue'
 import Register from '../views/Register.vue'
 import Categories from '../views/Categories.vue'
 import DetailRecord from '../views/DetailRecord.vue'
@@ -11,32 +13,45 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
+    meta: { layout: 'main' },
     component: Home
   },
   {
     path: '/login',
     name: 'login',
-    meta: {layout: 'empty'},
+    meta: { layout: 'empty' },
     component: Login
   },
   {
     path: '/register',
     name: 'register',
-    meta: {layout: 'empty'},
+    meta: { layout: 'empty' },
     component: Register
   },
   {
     path: '/categories',
     name: 'categories',
-    meta: {layout: 'main'},
+    meta: { layout: 'main' },
     component: Categories
   },
   {
     path: '/detail-record',
     name: 'detail-record',
-    meta: {layout: 'main'},
+    meta: { layout: 'main' },
     component: DetailRecord
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: History
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: Planning
   }
 ]
 
