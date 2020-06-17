77313ad94ba0af191655c7807bce97634670a6dd
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-17 20:23:24 +0300

    Create two layouts(empty, main). Connect login and categories page
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/router/index.js
+++ crm-system-vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Categories from '../views/Categories.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +12,18 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: Login
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: Categories
+  }
 ]
 
 const router = new VueRouter({
