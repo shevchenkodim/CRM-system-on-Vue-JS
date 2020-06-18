c33520038b07da0e399b8bf44bd1706ee0bfd908
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-18 06:04:10 +0300

    Code refactoring(use lazy load in routers)
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/router/index.js
+++ crm-system-vue/src/router/index.js
@@ -1,14 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Record from '../views/Record.vue'
-import History from '../views/History.vue'
-import Profile from '../views/Profile.vue'
-import Planning from '../views/Planning.vue'
-import Register from '../views/Register.vue'
-import Categories from '../views/Categories.vue'
-import DetailRecord from '../views/DetailRecord.vue'
 
 Vue.use(VueRouter)
 
@@ -17,55 +8,55 @@ const routes = [
     path: '/',
     name: 'home',
     meta: { layout: 'main' },
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/login',
     name: 'login',
     meta: { layout: 'empty' },
-    component: Login
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/register',
     name: 'register',
     meta: { layout: 'empty' },
-    component: Register
+    component: () => import('../views/Register.vue')
   },
   {
     path: '/categories',
     name: 'categories',
     meta: { layout: 'main' },
-    component: Categories
+    component: () => import('../views/Categories.vue')
   },
   {
     path: '/detail-record',
     name: 'detail-record',
     meta: { layout: 'main' },
-    component: DetailRecord
+    component: () => import('../views/DetailRecord.vue')
   },
   {
     path: '/history',
     name: 'history',
     meta: { layout: 'main' },
-    component: History
+    component: () => import('../views/History.vue')
   },
   {
     path: '/planning',
     name: 'planning',
     meta: { layout: 'main' },
-    component: Planning
+    component: () => import('../views/Planning.vue')
   },
   {
     path: '/profile',
     name: 'profile',
     meta: { layout: 'main' },
-    component: Profile
+    component: () => import('../views/Profile.vue')
   },
   {
     path: '/record',
     name: 'record',
     meta: { layout: 'main' },
-    component: Record
+    component: () => import('../views/Record.vue')
   }
 ]
 
