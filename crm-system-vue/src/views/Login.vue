2be7bccd7025e95fd4c1c1c4d334cfee723a998e
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-21 08:15:14 +0300

    Message plugin
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/views/Login.vue
+++ crm-system-vue/src/views/Login.vue
@@ -55,6 +55,7 @@
 
 <script>
 import { email, required, minLength } from "vuelidate/lib/validators";
+import messages from "@/utils/messages";
 
 export default {
   name: "login",
@@ -65,6 +66,11 @@ export default {
   validations: {
     email: { email, required },
     password: { required, minLength: minLength(6) }
+  },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message]);
+    }
   },
   methods: {
     submitHandler() {
