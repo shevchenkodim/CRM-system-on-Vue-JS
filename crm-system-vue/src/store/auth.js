d6b2e3093dc41356d6793c1b6c0ca266ff9a0be2
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-24 08:10:14 +0300

    Implement register user
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- crm-system-vue/src/store/auth.js
+++ crm-system-vue/src/store/auth.js
@@ -11,6 +11,23 @@ export default {
                 throw e
             }
         },
+        async register({ dispatch }, { email, password, name }) {
+            try {
+                await firebase.auth().createUserWithEmailAndPassword(email, password)
+                const uid = await dispatch('getUid')
+                await firebase.database().ref(`/users/${uid}/info`).set({
+                    bill: 1000,
+                    name
+                })
+            } catch (e) {
+                console.log(e);
+                throw e
+            }
+        },
+        getUid() {
+            const user = firebase.auth().currentUser
+            return user ? user.uid : null
+        },
         async logout() {
             await firebase.auth().signOut()
         }
--- crm-system-vue/src/views/Register.vue
+++ crm-system-vue/src/views/Register.vue
@@ -90,7 +90,7 @@ export default {
     agree: { checked: v => v }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -103,8 +103,12 @@ export default {
       };
 
       console.log(formData);
-
-      this.$router.push("/");
+      try {
+        await this.$store.dispatch("register", formData);
+        this.$router.push("/");
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 };
